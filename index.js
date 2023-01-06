467fadba5fa339f4e05fe56506d0e97295e18d5c
brightbrain20 <chibuzorbrightezeamaka@gmail.com>
2023-01-06 07:38:53 -0800

    complete popup for mobile and desktop

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+import Data from "./module/data.js";
+
+
 const mobileNavBtn = document.querySelector('.dropdown-img');
 const removeBtn = document.querySelector('.remove-btn');
 const mobileNav = document.querySelector('#mobile-navigation');
 const mobileNavLinks = document.querySelectorAll('.mobile-item');
+const popupContainer = document.querySelector('.popup');
+
 
 const navToggle = () => {
   mobileNav.classList.toggle('active');
@@ -14,3 +19,61 @@ removeBtn.addEventListener('click', navToggle);
 mobileNavLinks.forEach((link) => {
   link.addEventListener('click', navToggle);
 });
+
+
+const cardBtn = document.querySelectorAll ('.cards')
+cardBtn.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const btnId = btn.getAttribute('id') 
+    displayPopup(btnId)
+    const closePopupBtn = document.querySelector('.exit');
+    closePopupBtn.addEventListener('click', () => {
+      popupContainer.innerHTML = ''
+    })
+  })
+  
+});
+
+  const displayPopup = (id) => {
+    Data.forEach(data => {
+      const dataId = data.id;
+      if(dataId === id) {
+        popupContainer.innerHTML = `
+        <div class="popup2">
+        <div class="multi-container">
+          
+            <h2>${data.title}</h2>
+          
+          <div class="exit">x</div>
+        </div>
+        <ul class="language">
+          <li >html</li>
+          <li>bootstrap</li>
+          <li>Ruby on rails</li>
+        </ul>
+        <div class="content-container">
+          <div class="img">
+            <img src="${data.featured_image.mobile}" alt="img">
+          </div>
+          <div>
+            <p> ${data.description}</p>
+            <ul>
+              <li class="link-img">
+                <a href="">
+                  <img src="./Images/Enable 1.svg"  alt="img">
+                </a>
+              </li>
+              <li class="link-img ">
+                <a href="">
+                  <img src="./Images/Enable.svg" class="btn2" alt="img">
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+       
+      </div>
+        `
+      }
+    })
+  }
--- module/data.js
+++ module/data.js
@@ -1,142 +1,151 @@
-/* eslint-disable */
-export const projects = {
-    one: {
-      title: 'Multi-Post Stories',
-      description:
-        "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-      featured_image: {
-        mobile: './images/Img Placeholder.png',
-        desktop: './images/Img Placeholder (5).png',
-      },
-      technologies: [
-        'css',
-        'html',
-        'bootstrap',
-        'Ruby',
-      ],
-      links: {
-        live: '#',
-        source_code: '#',
-      },
+
+const Data = [
+  {
+    id: 'one',
+    title: 'Multi-Post Stories',
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    featured_image: {
+      mobile: './Images/Img Placeholder.png',
+      desktop: './Images/Img Placeholder (2).png',
     },
-    two: {
+    technologies: [
+      'css',
+      'html',
+      'bootstrap',
+      'Ruby',
+    ],
+    links: {
+      live: '#',
+      source_code: '#',
+    },
+  },
+  {
+    id: 'two',
+    title: 'Profesional Art Printing Data',
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    description1:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    featured_image: {
+      mobile: './Images/Img\ Placeholder\ \(1\).png',
+      desktop: './Images/Img\ Placeholder\ \(1\).png',
+    },
+    technologies: [
+      'html',
+      'bootstrap',
+      'Ruby',
+    ],
+    links: {
+      live: '#',
+      source_code: '#',
+    },
+  },
+  {
+    id: 'three',
+    title: 'Data Dashboard Healthcare',
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    description1:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    featured_image: {
+      mobile: './Images/Img Placeholder (4).png',
+      desktop: './Images/Img Placeholder (4).png',
+    },
+    technologies: [
+      'html',
+      'bootstrap',
+      'Ruby',
+    ],
+    links: {
+      live: '#',
+      source_code: '#',
+    },
+  },
+  {
+    id: 'four',
+      title: 'Data Dashboard Healthcare',
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    description1:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    featured_image: {
+      mobile: './Images/Img Placeholder (4).png',
+      desktop: './Images/Img Placeholder (4).png',
+    },
+    technologies: [
+      'html',
+      'bootstrap',
+      'Ruby',
+    ],
+    links: {
+      live: '#',
+      source_code: '#',
+    },
+  },
+  {
+    id: 'five',
       title: 'Profesional Art Printing Data',
-      description:
-        "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-      description1:
-        "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-      featured_image: {
-        mobile: './images/Img Placeholder (2).png',
-        desktop: './images/Img Placeholder (2).png',
-      },
-      technologies: [
-        'html',
-        'bootstrap',
-        'Ruby',
-      ],
-      links: {
-        live: '#',
-        source_code: '#',
-      },
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    description1:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    featured_image: {
+      desktop: './Images/Img\ Placeholder\ \(1\).png',
+      mobile: './Images/Img\ Placeholder\ \(1\).png',
     },
-    three: {
-      title: 'Profesional Art Printing Data',
-      description:
-        "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-      description1:
-        "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-      featured_image: {
-        mobile: './images/Img Placeholder (2).png',
-        desktop: './images/Img Placeholder (2).png',
-      },
-      technologies: [
-        'html',
-        'bootstrap',
-        'Ruby',
-      ],
-      links: {
-        live: '#',
-        source_code: '#',
-      },
+    technologies: [
+      'html',
+      'bootstrap',
+      'Ruby',
+    ],
+    links: {
+      live: '#',
+      source_code: '#',
     },
-    four: {
-      title: 'Profesional Art Printing Data',
-      description:
-        "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-      description1:
-        "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-      featured_image: {
-        mobile: './images/Img Placeholder (2).png',
-        desktop: './images/Img Placeholder (2).png',
-      },
-      technologies: [
-        'html',
-        'bootstrap',
-        'Ruby',
-      ],
-      links: {
-        live: '#',
-        source_code: '#',
-      },
+  },
+  {
+    id: 'six',
+        title: 'Data Dashboard Healthcare',
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    description1:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    featured_image: {
+      mobile: './Images/Img Placeholder (4).png',
+      desktop: './Images/Img Placeholder (4).png',
     },
-    five: {
-      title: 'Profesional Art Printing Data',
-      description:
-        "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-      description1:
-        "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-      featured_image: {
-        mobile: './images/Img Placeholder (2).png',
-        desktop: './images/Img Placeholder (2).png',
-      },
-      technologies: [
-        'html',
-        'bootstrap',
-        'Ruby',
-      ],
-      links: {
-        live: '#',
-        source_code: '#',
-      },
+    technologies: [
+      'html',
+      'bootstrap',
+      'Ruby',
+    ],
+    links: {
+      live: '#',
+      source_code: '#',
     },
-    six: {
-        title: 'Profesional Art Printing Data',
-        description:
-          "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-        description1:
-          "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-        featured_image: {
-          mobile: './images/Img Placeholder (2).png',
-          desktop: './images/Img Placeholder (2).png',
-        },
-        technologies: [
-          'html',
-          'bootstrap',
-          'Ruby',
-        ],
-        links: {
-          live: '#',
-          source_code: '#',
-        },
+  },
+  {
+    id: 'seven',
+      title: 'Data Dashboard Healthcare',
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    description1:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+    featured_image: {
+      mobile: './Images/Img Placeholder (4).png',
+      desktop: './Images/Img Placeholder (4).png',
     },
-    seven: {
-      title: 'Profesional Art Printing Data',
-      description:
-        "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-      description1:
-        "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-      featured_image: {
-        mobile: './images/Img Placeholder (2).png',
-        desktop: './images/Img Placeholder (2).png',
-      },
-      technologies: [
-        'html',
-        'bootstrap',
-        'Ruby',
-      ],
-      links: {
-        live: '#',
-        source_code: '#',
-      },
+    technologies: [
+      'html',
+      'bootstrap',
+      'Ruby',
+    ],
+    links: {
+      live: '#',
+      source_code: '#',
     },
-  };
+  },
+];
+
+export default Data
